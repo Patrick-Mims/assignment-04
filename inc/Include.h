5c6045a8bedb8f067418b476281df23cb2371ad9
Patrick Mims <patrickkevinmims@gmail.com>
2021-07-17 18:35:03 -0700

    Added Necessary Function Prototypes

--- inc/Include.h
+++ inc/Include.h
@@ -1,1 +1,23 @@
-//	Include.h
+#ifndef _INCLUDE_H
+#define _INCLDUE_H
+
+class AssignmentFour
+{
+private:
+public:
+    /*0*/ void addEnd340();
+    /*1*/ void removeRandom340();
+    /*2*/ void removeSecondNode340();
+
+    /* Curent Size */
+    /*3*/ void getCurrentSize340Iterative();
+    /*4*/ void getCurrentSize340Recursive();
+    /*5*/ void getCurrentSize340RecursiveHelper();
+    /*6*/ void getCurrentSize340RecursiveNoHelper(); // Immediate Recursion
+
+    /* Frequency */
+    /*7*/ void getFrequencyOf340Recursive();
+    /*8*/ void getFrequencyOf340RecursiveHelper();
+    /*9*/ void getFrequencyOf340RecursiveNoHelper(); // Immediate Recursion
+};
+#endif
