7a2192fe2ee8fd800b8e4e55aabae6e73720253f
Patrick Mims <patrickkevinmims@gmail.com>
2021-07-20 10:53:34 -0700

    Implementing Recursion

--- src/LinkedBag340.cpp
+++ src/LinkedBag340.cpp
@@ -8,7 +8,7 @@
 
     everything above this line is what the file started out as */
 
-#include "LinkedBag.h"
+#include "Include.h"
 
 /* point to the headPtr */
 template <class ItemType>
@@ -58,11 +58,33 @@ int LinkedBag<ItemType>::getCurrentSize340Iterative() const
 /* counts the number of nodes in the Linked Bag recursively. 
    Use 1 helper function: getCurrentSize340RecursiveHelper() */
 template <class ItemType>
-int LinkedBag<ItemType>::getCurrentSize340Recursive() const {}
+int LinkedBag<ItemType>::getCurrentSize340Recursive() const
+{
+
+    int counter = 0, frequency = 0;
+    /*  Initially, curPtr must point to the first node. 
+        Because headPtr points to the first node, simply 
+        copy headPtr into currPtr by writing */
+    Node<ItemType> *curPtr = headPtr;
+
+    while ((curPtr != nullptr) && (counter < itemCount))
+    {
+        /* ...advance the current pointer to the next node */
+        curPtr = curPtr->getNext();
+        counter += 1;
+    }
+
+    return counter;
+}
 
 /* helper function */
 template <class ItemType>
-int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType> *) const {}
+int LinkedBag<ItemType>::getCurrentSize340RecursiveHelper(Node<ItemType> *element) const
+{
+    cout << "Get Current Size 340 Recursive Helper: " << endl;
+
+    return element->getNext();
+}
 
 /* counts the number of nodes in the Linked Bag recursively. 
    This recursive function does not use any helper functions */
@@ -70,19 +92,45 @@ template <class ItemType>
 int LinkedBag<ItemType>::getCurrentSize340RecursiveNoHelper() const {}
 
 /* recursively counts the number of times an entry appears in the Linked Bag. 
-   Use 1 helper function: getFrequencyOf340Recursive */
+   Use 1 helper function: getFrequencyOf340RecursiveHelper */
 template <class ItemType>
 int LinkedBag<ItemType>::getFrequencyOf340Recursive(const ItemType &anEntry) const
 {
+    /*  Initially, curPtr must point to the first node. 
+        Because headPtr points to the first node, simply 
+        copy headPtr into currPtr by writing */
+
     int counter = 0, frequency = 0;
-    /* Initially, curPtr */
+
     Node<ItemType> *curPtr = headPtr;
 
+    /*
     while ((curPtr != nullptr) && (counter < itemCount))
     {
+        if (anEntry == curPtr->getItem())
+        {
+            frequency += 1;
+        }
+
+        counter += 1;
+        curPtr = curPtr->getNext();
+    }
+    */
+
+    //curPtr = curPtr->getNext();
+    // keep calling getCurrentSize340RecursiveHelper();
+    if (anEntry == curPtr->getItem())
+    {
+        frequency++;
+    }
+    else
+    {
+        frequency = getCurrentSize340RecursiveHelper(curPtr);
     }
 
-    return counter;
+    counter++;
+
+    return frequency;
 }
 
 /* helper function */
@@ -92,25 +140,20 @@ int LinkedBag<ItemType>::getFrequencyOf340RecursiveHelper(Node<ItemType> *, cons
 /* recursively counts the number of times an entry appears in the Linked Bag.
     This recursive function does not use any helper functions */
 template <class ItemType>
-int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType &) const {}
+int LinkedBag<ItemType>::getFrequencyOf340RecursiveNoHelper(const ItemType &entry) const
+{
+    /*  Initially, curPtr must point to the first node. 
+        Because headPtr points to the first node, simply 
+        copy headPtr into currentPtr by writing */
+    Node<ItemType> *currentPtr = headPtr;
+
+    /*
+    while ((currentPtr != nullptr) && (counter < itemCount))
+    {
+    }
+    */
+}
 
 /* removes a random entry from the Linked Bag */
 template <class ItemType>
 ItemType LinkedBag<ItemType>::removeRandom340() {}
-
-/*
-template <class ItemType>
-std::vector<ItemType> LinkedBag<ItemType>::toVector() const
-{
-    vector<ItemType> bagContents;
-    Node<ItemType> *curPtr = headPtr;
-    int counter = 0;
-    while ((curPtr != nullptr) && (counter < itemCount))
-    {
-        bagContents.push_back(curPtr->getItem());
-        curPtr = curPtr->getNext();
-        counter++;
-    }
-    return bagContents;
-}
-*/
